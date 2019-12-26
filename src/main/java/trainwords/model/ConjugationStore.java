8af1ca18265802dd7cfa0bd82457845b670efb2c
osavenko <osavenko@gmail.com>
2019-12-26 22:12:17 +0200

    Edit class ConjugationStore.

--- src/main/java/trainwords/model/ConjugationStore.java
+++ src/main/java/trainwords/model/ConjugationStore.java
@@ -6,4 +6,28 @@ import java.util.List;
 public class ConjugationStore {
     private List<Conjugation> lists = new ArrayList<Conjugation>();
 
+    public ConjugationStore() {
+    }
+
+    public ConjugationStore(List<Conjugation> lists) {
+        this.lists = lists;
+    }
+    public void addConjugation(Conjugation conjugation){
+        lists.add(conjugation);
+    }
+    public void setConjugationStore(List<Conjugation> lists){
+        this.lists = lists;
+    }
+    public List<Conjugation> getConjugationStore(){
+        return this.lists;
+    }
+    public Conjugation getConjugationByIndex(int id){
+        if(id>lists.size()-1&&id<0){
+            throw new IndexOutOfBoundsException();
+        }
+        return lists.get(id);
+    }
+    public int size(){
+        return lists.size();
+    }
 }
