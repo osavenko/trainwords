834bb913e0995f1773179e958ecdd2b410af449e
osavenko <osavenko@gmail.com>
2019-12-26 21:58:32 +0200

    Add class ConjugationStore.

---
+++ src/main/java/trainwords/model/ConjugationStore.java
@@ -0,0 +1,9 @@
+package trainwords.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConjugationStore {
+    private List<Conjugation> lists = new ArrayList<Conjugation>();
+
+}
